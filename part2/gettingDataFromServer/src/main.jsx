d69100c0691782145857d9fec492e8e938bc7734
Hashley Cuachin <hashleycuachin03@gmail.com>
2026-09-03 14:33:58 +0800

    added db.json, modified main.jsx, package.json and app.jsx for fetching data from db.json to the react app

--- part2/gettingDataFromServer/src/main.jsx
+++ part2/gettingDataFromServer/src/main.jsx
@@ -1,8 +1,5 @@
-import ReactDOM from 'react-dom/client'
-import axios from 'axios'
-import App from './App'
 
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
-})
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
