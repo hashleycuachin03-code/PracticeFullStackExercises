5c264aa3c94caf5be7d73d3fbe7cdfb193b11e23
Hashley Cuachin <hashleycuachin03@gmail.com>
2026-09-05 10:47:52 +0800

    modified app.jsx for .post method go to db.json server

--- part2/gettingDataFromServer/src/App.jsx
+++ part2/gettingDataFromServer/src/App.jsx
@@ -32,7 +32,8 @@ const App = () => {
     axios
       .post('http://localhost:3001/notes', noteObject)
       .then(response => {
-        console.log(response)
+        setNotes(notes.concat(response.data))
+        setNewNote('')
       })
   }
 
